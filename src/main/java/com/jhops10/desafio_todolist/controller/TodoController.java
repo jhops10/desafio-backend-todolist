a7731adeac7b5c7e359fa2cc7ed864af63194c18
João Paulo <joaowais@gmail.com>
2024-09-23 18:10:35 -0300

    Cria controller e os metodos CRUD

---
+++ src/main/java/com/jhops10/desafio_todolist/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.jhops10.desafio_todolist.controller;
+
+import com.jhops10.desafio_todolist.entity.Todo;
+import com.jhops10.desafio_todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    public List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    public List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    public List<Todo> delete(@PathVariable Long id) {
+        return todoService.delete(id);
+    }
+
+}
